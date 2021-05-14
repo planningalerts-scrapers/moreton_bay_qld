108a79b25ff1e0f2a7a7446b8dca9d3f989d28b4
Matthew Landauer <mlandauer@gmail.com>
2021-05-15 08:59:21 +1000

    Get all applications submitted in the last 28 days

--- scraper.rb
+++ scraper.rb
@@ -5,12 +5,16 @@ url = "https://api.moretonbay.qld.gov.au/mplu/da/search/advanced"
 start_date = "2021-05-01 00:00 +10:00"
 end_date = "2021-05-08 00:00 +10:00"
 
+# All application submitted in the last 28 days
+end_date = Time.now
+start_date = end_date - 28 * 24 * 60 * 60
+
 query = {
   searchType: "advanced",
   propertyType: "address",
   dateRange: "custom",
-  start: start_date,
-  end: end_date
+  start: start_date.to_s,
+  end: end_date.to_s
 }
 
 pp HTTParty.get(url, query: query)
